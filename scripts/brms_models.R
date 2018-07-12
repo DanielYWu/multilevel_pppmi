f8fd9f8d8ae998d5647d2df55a4adc3de9831fa7
Daniel <wuxx1495@umn.edu>
2018-07-12 00:17:31 -0500

    updated preprocessing to add UPDRS scores and started exploratory plots

---
+++ scripts/brms_models.R
@@ -0,0 +1,6 @@
+library(dplyr)
+library(ggplot2)
+library(here)
+library(brms)
+vignette(package = "brms")
+brms_overview 
--- scripts/preprocess.R
+++ scripts/preprocess.R
@@ -23,12 +23,14 @@ pd_df <-  diagTable %>%
 pd_df <-  pd_df %>% left_join(pd_diag, c("PATNO"))
 pd_df <- pd_df %>% left_join(drug_init, c("PATNO"))
 pd_df <- pd_df %>% left_join(surgTable, c("PATNO"))
+
+# Need a better way of dealing with duplicate files
 pd_df <- pd_df %>% distinct(PATNO, EVENT_ID, .keep_all = TRUE)
 
 pd_df <- pd_df %>% 
   mutate(dateDX = as.Date(paste0(PDDXDT, "-01"), "%b-%y-%d")) %>% 
   mutate(dateDrug =  as.Date(paste0(INITMDDT, "-01"), "%b-%y-%d")) %>% 
-  mutate(dateSurg =  as.Date(paste0(PDSURGDT, "-01"), "%m/%Y-%d")) %>%
+  mutate(dateSurg =  as.Date(paste0(PDSURGDT, "-01"), "%b-%y-%d")) %>%
   mutate(dateVisit = as.Date(paste0(ORIG_ENTRY, "-01"), "%m/%Y-%d")) %>%
   arrange(PATNO, dateVisit)
 
@@ -36,11 +38,28 @@ date_diff <- pd_df %>%
   mutate(visit_time= as.numeric(dateVisit - dateDX)) %>%
   mutate(drug_time= as.numeric(dateDrug - dateDX)) %>%
   mutate(surg_time= as.numeric(dateSurg - dateDX))
-# Number of Distinct Patients with PD (PRIMDIAG = 1)
+
+# Number of Disdattinct Patients with PD (PRIMDIAG = 1) (n = 413)
 nrow(distinct(pd_df,PATNO))
 
-updrsTable = read.csv(here("data-raw","MDS_UPDRS_total_score.csv"), header=T)
+# Number of patients who got DBS (n = 13)
+nrow(distinct(date_diff %>% filter(PDSURG == 1),PATNO) )
 
+# Calculate the UPDRS totals for each UPDRS part
+
+updrs1Table = read.csv(here("data-raw","download","MDS_UPDRS_Part_I.csv"), header=T, stringsAsFactors=FALSE)
+updrs2Table = read.csv(here("data-raw","download","MDS_UPDRS_Part_II__Patient_Questionnaire.csv"), header=T, stringsAsFactors=FALSE)
+updrs3Table = read.csv(here("data-raw","download","MDS_UPDRS_Part_III__Post_Dose_.csv"), header=T, stringsAsFactors=FALSE)
+updrs4Table= read.csv(here("data-raw","download", "MDS_UPDRS_Part_IV.csv"), header=T, stringsAsFactors=FALSE)
+
+updrs1Table <- updrs1Table %>% mutate(total1=NP1COG + NP1HALL + NP1DPRS + NP1ANXS + NP1APAT + NP1DDS ) %>% 
+  select(PATNO, EVENT_ID, total1) #Max score for part I is 24
+updrs2Table <- updrs2Table %>% mutate(total2=NP2SPCH + NP2SALV + NP2SWAL +NP2EAT + NP2DRES + NP2HYGN + NP2HWRT+ NP2HOBB+ NP2TURN+ NP2TRMR + NP2RISE+ NP2WALK+ NP2FREZ) %>%
+  select(PATNO, EVENT_ID, total2) #Max score for part I is 24
+updrs3Table <- updrs3Table %>% mutate(total3=rowSums(select(.,contains("NP3")))) %>% 
+  select(PATNO, EVENT_ID, total3) #Max score for part I is 24
+updrs4Table <- updrs4Table %>% mutate(total4=NP4WDYSK + NP4DYSKI + NP4OFF +NP4FLCTI + NP4FLCTX + NP4DYSTN) %>% 
+  select(PATNO, EVENT_ID, total4) #Max score for part I is 24
 medTable = read.csv(here("data-raw","Use_of_PD_Medication.csv"), header=T)
 updrsTable1 = read.csv(here("data-raw","MDS_UPDRS_PartI_score.csv"), header=T)
 pd_initiation = read.csv(here("data-raw","Initiation_of_PD_Medication.csv"), header=T)
