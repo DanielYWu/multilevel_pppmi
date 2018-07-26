01007173e401ea832f2e56243035753334cd4fd2
Daniel <wuxx1495@umn.edu>
2018-07-26 06:40:27 -0500

    Updated scripts for nonlinear modeling

--- scripts/brms_models.R
+++ scripts/brms_models.R
@@ -3,4 +3,46 @@ library(ggplot2)
 library(here)
 library(brms)
 vignette(package = "brms")
-brms_overview 
+
+scaled_df <- final_df %>% mutate_each_(funs(scale),vars=c("visit_time","drug_time","updrsT","age"))
+
+
+row_to_remove <- which(final_df$PATNO == 3478)
+final_df <- final_df[-row_to_remove,]
+surg.m1 <- brm(updrsT ~ (1|PATNO) + (1|CNO)
+                 + visit_time + visit_time:age + PDSURG,
+                 data=scaled_df,
+                 prior=c(set_prior("normal(0,10)", class="Intercept"),
+                         set_prior("normal(0,10)", class='b'),
+                         set_prior("cauchy(0,1)", class='sd', group="PATNO"),
+                         set_prior("cauchy(0,1)", class='sd', group="CNO")))
+
+bayes.mod <- brm(updrsT ~ (1|PATNO) + (1|CNO)
+                 + visit_time + visit_time:age,
+                 data=final_df,
+                 prior=c(set_prior("normal(0,10)", class="Intercept"),
+                         set_prior("normal(0,10)", class='b'),
+                         set_prior("cauchy(0,1)", class='sd', group="PATNO"),
+                         set_prior("cauchy(0,1)", class='sd', group="CNO")))
+
+nd <- 
+  tibble(population = seq(from = 1000, to = 300000, by = 5000),
+         # To "simulate counterfactual societies, using the hyper-parameters" (p. 383), we'll plug a new island into the `culture` variable
+         culture = "My_island") summ
+
+p1 <- predict(bayes.mod,
+              allow_new_levels = T,
+              data = final_df,
+              re_formula = ~ (1 | PATNO))
+
+
+ggplot(final_df, aes(visit_time, Estimate,col= PDSURG )) + geom_line(alpha = 0.5,aes(group= factor(PATNO))) + ggtitle("UPDRS totals over time")
+
+# Model where we assume that as time goes on that the slope of visit_time will be different depending on the 
+pred1 <- brm(updrsT ~ (visit_time|PATNO) + (1|CNO) + 
+                   + visit_time + visit_time:age,
+                 data=final_df,
+                 prior=c(set_prior("normal(0,10)", class="Intercept"),
+                         set_prior("normal(0,10)", class='b'),
+                         set_prior("cauchy(0,1)", class='sd', group="PATNO"),
+                         set_prior("cauchy(0,1)", class='sd', group="CNO")))
--- scripts/preprocess.R
+++ scripts/preprocess.R
@@ -7,6 +7,8 @@ surgTable = read.csv(here("data-raw","download","Surgery_for_Parkinson_Disease.c
 centerTable = read.csv(here("data-raw","download","Center-Subject_List.csv"), header=T, stringsAsFactors=FALSE)
 drug_init = read.csv(here("data-raw","download","Initiation_of_PD_Medication-_incidents.csv"), header=T, stringsAsFactors=FALSE)
 pd_diag= read.csv(here("data-raw","download", "PD_Features.csv"), header=T, stringsAsFactors=FALSE)
+demog= read.csv(here("data-raw","download", "Screening___Demographics.csv"), header=T, stringsAsFactors=FALSE) %>%
+  select(PATNO, BIRTHDT, GENDER)
 
 drug_init <-drug_init %>% select(PATNO, INITMDDT)
 pd_diag <- pd_diag %>% select(PATNO, PDDXDT)
@@ -25,7 +27,7 @@ pd_df <- pd_df %>% left_join(drug_init, c("PATNO"))
 pd_df <- pd_df %>% left_join(surgTable, c("PATNO"))
 
 # Need a better way of dealing with duplicate files
-pd_df <- pd_df %>% distinct(PATNO, EVENT_ID, .keep_all = TRUE)
+# pd_df <- pd_df %>% distinct(PATNO, EVENT_ID, .keep_all = TRUE)
 
 pd_df <- pd_df %>% 
   mutate(dateDX = as.Date(paste0(PDDXDT, "-01"), "%b-%y-%d")) %>% 
@@ -55,25 +57,48 @@ updrs4Table= read.csv(here("data-raw","download", "MDS_UPDRS_Part_IV.csv"), head
 updrs1Table <- updrs1Table %>% mutate(total1=NP1COG + NP1HALL + NP1DPRS + NP1ANXS + NP1APAT + NP1DDS ) %>% 
   select(PATNO, EVENT_ID, total1) #Max score for part I is 24
 updrs2Table <- updrs2Table %>% mutate(total2=NP2SPCH + NP2SALV + NP2SWAL +NP2EAT + NP2DRES + NP2HYGN + NP2HWRT+ NP2HOBB+ NP2TURN+ NP2TRMR + NP2RISE+ NP2WALK+ NP2FREZ) %>%
-  select(PATNO, EVENT_ID, total2) #Max score for part I is 24
+  select(PATNO, EVENT_ID, total2) #Max score for part II is 24
 updrs3Table <- updrs3Table %>% mutate(total3=rowSums(select(.,contains("NP3")))) %>% 
-  select(PATNO, EVENT_ID, total3) #Max score for part I is 24
+  select(PATNO, EVENT_ID, total3) #Max score for part III is 24
 updrs4Table <- updrs4Table %>% mutate(total4=NP4WDYSK + NP4DYSKI + NP4OFF +NP4FLCTI + NP4FLCTX + NP4DYSTN) %>% 
-  select(PATNO, EVENT_ID, total4) #Max score for part I is 24
-medTable = read.csv(here("data-raw","Use_of_PD_Medication.csv"), header=T)
-updrsTable1 = read.csv(here("data-raw","MDS_UPDRS_PartI_score.csv"), header=T)
-pd_initiation = read.csv(here("data-raw","Initiation_of_PD_Medication.csv"), header=T)
-pd_diag= read.csv(here("data-raw","pd_features01.csv"), header=T)
-pd_feat =read.csv(here("data-raw","pd_features.csv"), header=T)
-demo = read.csv(here("data-raw","Demographics.csv"), header=T)
-prodromal = read.csv(here("data-raw","Prodromal_Diagnostic_Questionnaire.csv"), header = T)
-# Potential features to look out for 
-gen_neuro = read.csv("ClinicalData/General_Neurological_Exam.csv")
-gen_phys = read.csv("ClinicalData/General_Physical_Exam.csv")
+  select(PATNO, EVENT_ID, total4) #Max score for part IV is 24
+
+final_df <- date_diff %>%
+  left_join(updrs1Table, c("PATNO", "EVENT_ID")) %>%
+  left_join(updrs2Table, c("PATNO", "EVENT_ID")) %>%
+  left_join(updrs3Table, c("PATNO", "EVENT_ID")) %>%
+  left_join(updrs4Table, c("PATNO", "EVENT_ID"))
+
+# Convert NAs in these columns to zeros
+final_df[,c("total1", "total2", "total3", "total4")][is.na(final_df[c("total1", "total2", "total3", "total4")])] <- 0
+
+final_df <- final_df %>%
+  mutate(updrsT = total1 + total2 + total3 + total4)
+
+final_df[,c("PDSURG","PDSURGTP")][is.na(final_df[c("PDSURG","PDSURGTP")])] <- 0
+
+final_df <- final_df %>%
+  mutate(PATNO = factor(PATNO)) %>%
+  mutate(CNO = factor(CNO)) %>%
+  left_join(demog, c("PATNO")) %>%
+  mutate(PATNO = factor(PATNO))
+  
+
+final_df <- final_df %>%
+  mutate(age = as.numeric(dateDX - as.Date(paste0(BIRTHDT, "-01-01"), "%Y-%m-%d")) / 365)
 
 
+final_df <- final_df %>%
+  mutate(PDSURG = factor(PDSURG))
+# Need a better way of dealing with duplicate files
+final_df <- final_df %>% distinct(PATNO, EVENT_ID, .keep_all = TRUE)
+ggplot(final_df, aes(visit_time, updrsT,col= PDSURG )) + geom_line(alpha = 0.5,aes(group= factor(PATNO))) + ggtitle("UPDRS totals over time")
+ggplot(final_df, aes(visit_time, total1,col= PDSURG )) + geom_line(alpha = 0.5,aes(group= factor(PATNO))) + ggtitle("UPDRS PART I totals over time")
+ggplot(final_df, aes(visit_time, total2,col= PDSURG )) + geom_line(alpha = 0.5,aes(group= factor(PATNO)))+ ggtitle("UPDRS PART II totals over time")
+ggplot(final_df, aes(visit_time, total3,col= PDSURG )) + geom_line(alpha = 0.5,aes(group= factor(PATNO)))+ ggtitle("UPDRS PART III totals over time")
 
-
+ggplot(final_df, aes(visit_time, updrsT,col= age )) + geom_line(alpha = 0.5,aes(group= factor(PATNO))) + ggtitle("UPDRS totals over time")
+ggplot(final_df, aes(visit_time, updrsT,col= factor(GENDER) )) + geom_line(alpha = 0.5,aes(group= factor(PATNO))) + ggtitle("UPDRS totals over time")
 
 
 # Need to convert each visit into a month, probably a better way to do this, but for now this is what I could figure out
